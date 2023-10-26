6343526fd20e599948aa4e93111b0ff83220b431
bit901 <rg.santha@gmail.com>
2023-10-26 10:39:37 -0700

    learning firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDFrwudOPZr41kJEQezmCs35Q86gf2qvZA",
+  authDomain: "learn-react-firebase-158db.firebaseapp.com",
+  projectId: "learn-react-firebase-158db",
+  storageBucket: "learn-react-firebase-158db.appspot.com",
+  messagingSenderId: "998765757628",
+  appId: "1:998765757628:web:e1ace3cea1c5e81f667cb9",
+  measurementId: "G-WJ4MFDC0NB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
