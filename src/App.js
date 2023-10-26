731cc0129fdbae08f82c5a4a3a4ec923b1a8d9b7
bit901 <rg.santha@gmail.com>
2023-10-26 15:17:55 -0700

    firebase learning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import './App.css';
 import { Auth } from "./components/auth"
+import { db } from "./config/firebase"
+import { useState, useEffect } from "react"
+import { getDocs, collection } from "firebase/firestore"
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  const moviesCollectionReference = collection(db, "movies");
+
+  
+  useEffect(() => {
+    const getMovieList = async () => {
+      // read data
+
+      // set movie list
+      try {
+        const data = await getDocs(moviesCollectionReference);
+      } catch (err) {
+        console.error(err);
+      }
+
+    }}, [])
+  
+
   return (
     <div className="App">
       <Auth />
