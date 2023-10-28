0c9747f591cf3f8f49967a14eb31f49a1cf4cb7a
bit901 <rg.santha@gmail.com>
2023-10-28 08:45:07 -0700

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import { Auth } from "./components/auth"
-import { db, auth } from "./config/firebase"
+import { db, auth, storage} from "./config/firebase"
 import { useState, useEffect } from "react"
 import { getDocs, collection, addDoc, deleteDoc, doc, updateDoc } from "firebase/firestore"
+import { ref, uploadBytes } from 'firebase/storage';
 
 function App() {
   const [movieList, setMovieList] = useState([]);
@@ -10,7 +11,8 @@ function App() {
   // new movie states
   const [newMovieTitle, setNewMovieTitle] = useState("")
   const [newReleaseDate, setNewReleaseDate] = useState(0)
-  const [isNewMovieOscar, setisNewMovieOscar] = useState(false)
+  const [isNewMovieOscar, setisNewMovieOscar] = useState(false);
+  const [fileUpload, setFileUpload] = useState(null)
 
   // update title state
   const [updatedTitle, setUpdatedTitle] = useState("")
@@ -35,7 +37,7 @@ function App() {
   
   useEffect(() => {
     getMovieList();
-  }, []);
+  }, [getMovieList]);
 
   const onSubmitMovie = async () => {
     try {
@@ -58,6 +60,16 @@ function App() {
   const updateMovieTitle = async (id, ) => {
     const movieDoc = doc(db, "movies", id);
     await updateDoc(movieDoc, {title: updatedTitle});
+  };
+
+  const uploadFile = async () => {
+    if (!fileUpload) return;
+    const filesFolderRef = ref(storage, `projectFiles/${fileUpload.name}`)
+    try {
+      await uploadBytes(filesFolderRef, fileUpload);
+    } catch(err) {
+      console.error(err);
+    }
   };
 
   return (
@@ -87,6 +99,11 @@ function App() {
           </div>
         ))}
       </div>
+      <div>
+
+      </div>
+        <input type='file' onChange={(e) => setFileUpload(e.target.files[0])}/>
+        <button onClick={uploadFile}>Upload File</button>
 
     </div>
   );
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
-
+import { getStorage } from "firebase/storage"
 
 
 
@@ -28,3 +28,4 @@ const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+export const storage = getStorage(app);
